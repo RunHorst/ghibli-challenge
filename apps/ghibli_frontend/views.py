4e0a555346284823dc95adccf06942c635353963
Mario Mohr <mario.mohr@viasto.com>
2020-08-19 17:23:07 +0200

    add dummy template for movie list

--- apps/ghibli_frontend/views.py
+++ apps/ghibli_frontend/views.py
@@ -1,16 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from apps.ghibli_api_adapter.lib import retrieve_movie_list
 
 
 def movies_overview(request):
-    movie_list = [
-        f'<li>{movie}</li>'
-        for movie in retrieve_movie_list()
-    ]
+    movie_list = retrieve_movie_list()
 
-    return HttpResponse(
-        f'<h3>Ghibli movies:</h3>'
-        f'<ul>{"".join(movie_list)}</ul>'
-        f'<p>Hier könnte Ihre Werbung stehen.</p>'
+    return render(
+        request,
+        'movie-list/index.html',
+        {
+            'movies': movie_list
+        }
     )
