54d5a9bb48858165d368300275c31bb5ead24114
Mario Mohr <mario.mohr@viasto.com>
2020-08-20 17:48:45 +0200

    extend prod settings:  - ALLOWED_HOSTS  - proper-ish SECRET_KEY

--- ghibli_challenge_django/settings/settings_prod.py
+++ ghibli_challenge_django/settings/settings_prod.py
@@ -1,3 +1,18 @@
 from .settings_base import *
 
-DEBUG=False
+import os
+import secrets
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    # My personal domain; feel free to replace with your own
+    'ghibli.runhorst.dev'
+]
+
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    # We don't have any persistent data anyway, so we can just
+    # create a secret key at runtime if needed
+    secrets.random_hex(32)
+)
